bf65ffa2614f58c36d444c2527577d295ceef1f7
Diệp Tấn Luân <82451887+dieptanluangithub@users.noreply.github.com>
2023-06-02 06:23:19 +0700

    Initial commit

---
+++ lib/utils/global_variables.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/screens/add_post_screen.dart';
+
+import '../screens/feed_screen.dart';
+import '../screens/profile_screen.dart';
+import '../screens/search_screen.dart';
+import '../screens/video_screen.dart';
+
+const webScreenSize = 600;
+
+List<Widget> homeScreenItems = [
+  const FeedScreen(),
+  const SearchScreen(),
+  const AddPostScreen(),
+  VideoScreen(),
+  ProfileScreen(
+    uid: FirebaseAuth.instance.currentUser!.uid,
+  ),
+];
